20bf55be008aae56dda96bc93960c8605d6a09c5
Casey Childers <cachilders@gmail.com>
2016-10-10 08:27:32 -0700

    [bug out] Delete breaking code repetition

--- server/db-config.js
+++ server/db-config.js
@@ -1,22 +1,5 @@
 var mongoose = require('mongoose');
 var path = require('path');
-
-// In the interest of cleanliness, a db-schemas directory can be found in the server directory
-
-mongoURI = process.env.MONGODB_URI || 'mongodb://localhost/abrdgd'
-mongoose.connect(mongoURI);
-
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'Mongoose did wet the bed.'));
-db.once('open', function () {
-  console.log('Mongoose is up and running.')
-});
-
-module.exports = db;
-
-
-var mongoose = require('mongoose');
-var path = require('path');
 var request = require('request');
 
 mongoURI = process.env.MONGODB_URI || 'mongodb://localhost/worst'
